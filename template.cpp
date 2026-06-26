eb90446ead71642c91613e732ebbc39e03c4a244
smizuno516-cyber <s.mizuno.516@icloud.com>
2026-06-26 13:02:03 +0900

    Added template for C++ program with common utilities

---
+++ template.cpp
@@ -0,0 +1,28 @@
+import std;
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vi>;
+using vvll = vector<vll>;
+using vc = vector<char>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+#define rep(i, N) for (ll i = 0; i < (ll)(N); ++i)
+#define rep3(i, M, N) for (ll i = (ll)(M); i < (ll)(N); ++i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#ifdef LOCAL
+#define debug(x) println(cerr, "{} = {}", #x, x)
+#else
+#define debug(x)
+#endif
+constexpr int mod998 = 998244353;
+constexpr int mod107 = 1000000007;
+
+int main() {
+    cin.tie(nullptr);
+    
+    
+}
