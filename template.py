86b50505be0a87d9b922a76e95b83ff41bb5ce80
london1957 (AtCoder) <s.mizuno.516@icloud.com>
2026-08-12 10:27:33 +0900

    Add template.py with basic setup and functions

---
+++ template.py
@@ -0,0 +1,25 @@
+import sys
+sys.setrecursionlimit(10**7)
+sys.set_int_max_str_digits(0)
+from collections import deque, Counter
+from heapq import heappush, heappop
+from itertools import permutations, combinations
+import bisect
+import math
+input = lambda: sys.stdin.readline().rstrip()
+def INT(): return int(input())
+def STR(): return input()
+def MAP(): return map(int, input().split())
+def LIST(): return list(map(int, input().split()))
+def MAT(N): return [LIST() for _ in range(N)]
+yn = lambda ans: print("Yes" if ans else "No")
+mod998 = 998244353
+mod107 = 1000000007
+inf = float('inf')
+
+def main():
+    
+    pass
+
+if __name__ == "__main__":
+    main()
